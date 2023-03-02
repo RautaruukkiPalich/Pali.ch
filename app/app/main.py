101c092626c2d16c95400593fe10d7685fc2a472
RautaruukkiPalich <rautaruukkipalich@gmail.com>
2023-03-03 00:51:09 +0500

    add static

--- app/app/main.py
+++ app/app/main.py
@@ -1,37 +1,44 @@
-import datetime
 import uvicorn
 import logging
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from src.db.redisDB.redis_settings import start_redis
 from src.services import routers
+from datetime import datetime as dt
 
 
 app = FastAPI()
+app.include_router(routers.router)
+app.mount('/static', StaticFiles(directory='../../src/static/'), name='static')
+
+log_path = "../../log/"
+
 logging.basicConfig(
     level=logging.DEBUG,
-    filename="..//../log/main_log.log",
+    filename=f"{log_path}main_log.log",
 )
-
-
-@app.get("/")
-async def index():
-    return {"body": "main_page"}
-
-
-app.include_router(routers.router)
 
 
 @app.on_event("startup")
 async def on_startup():
-    logging.log(logging.INFO, f"{datetime.datetime.now()}: server up...")
+    logging.log(
+        logging.INFO,
+        f"{dt.now()}: server up...",
+    )
     await start_redis()
-    logging.log(logging.INFO, f"{datetime.datetime.now()}: redis up...")
+    logging.log(
+        logging.INFO,
+        f"{dt.now()}: redis up...",
+    )
 
 
 @app.on_event("shutdown")
 async def on_shutdown():
-    logging.log(logging.INFO, f"{datetime.datetime.now()}: server down... ")
+    logging.log(
+        logging.INFO,
+        f"{dt.now()}: server down... ",
+    )
 
 
 if __name__ == "__main__":
