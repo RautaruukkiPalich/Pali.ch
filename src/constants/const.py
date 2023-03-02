8db39542def7ccbb1af21d8823ce135771773cd0
RautaruukkiPalich <rautaruukkipalich@gmail.com>
2023-03-02 11:20:10 +0500

    add popular urls

--- src/constants/const.py
+++ src/constants/const.py
@@ -1,9 +1,16 @@
+import os
 from string import ascii_letters, digits
 
 SYMBOLS = ascii_letters + digits
 """
 HOST_URL = "https://pali.ch/
 """
-HOST_URL = "http://127.0.0.1:8000/"
+HOST_URL = os.environ.get(f"HOST_URL", "localhost:8000/")
 
-POPULAR_URLS = ["vk.com", "google.com",]
+POPULAR_URLS = [
+    "vk.com",
+    "google.com",
+    "wikipedia",
+    "yandex",
+    "ya.ru",
+]
