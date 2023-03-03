0ec07581fa5cbcdebf4334c5dd086077176fc7b7
RautaruukkiPalich <rautaruukkipalich@gmail.com>
2023-03-03 10:44:17 +0500

    add regex filter to /create replace GET for / add template path

--- src/services/routers.py
+++ src/services/routers.py
@@ -1,11 +1,27 @@
 import json
+import re
 
-from fastapi import APIRouter, Response, Body
-from starlette.responses import RedirectResponse
+from fastapi import APIRouter, Response, Body, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+
 from src.constants.const import HOST_URL
 from src.app.funcs import create_short_url, check_short_url, check_long_url
+from src.templates.index import get_templates
+from src.constants.const import ROOT
+
 
 router = APIRouter()
+templates = get_templates(f"{ROOT}/src/templates/html/")
+
+
+@router.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+        },
+    )
 
 
 @router.get("/{url}", tags=["redirect"])
@@ -27,6 +43,11 @@ async def redirect_url(url: str):
 @router.post("/create/", tags=["shortly"])
 async def post_long_url(data: dict = Body()):
     long_url = data["long_url"]
+    regex = r"[-a-zA-Z0-9@:%_\+.~#?&\/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/=]*)?"
+    if not re.search(regex, long_url):
+        return Response(
+            status_code=422
+        )
     check, short_url = await check_long_url(long_url=long_url)
     if not check:
         short_url = await create_short_url(long_url=long_url)
