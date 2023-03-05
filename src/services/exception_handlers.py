d7ea167a045047640c10a425a608aa3afcb4d05f
RautaruukkiPalich <rautaruukkipalich@gmail.com>
2023-03-05 17:43:50 +0500

    add custom exception handler

--- src/services/exception_handlers.py
+++ src/services/exception_handlers.py
@@ -6,6 +6,10 @@ templates = get_templates(f"{ROOT}/src/templates/html/")
 
 
 async def exception_handler_404(request: Request):
+    """
+    :param request:
+    :return: Template 404.html
+    """
     return templates.TemplateResponse(
         "404.html",
         {
